44a818680a79d8e720b7ac2fcc1ef6053449dc87
MohammadFarzan <farzanmohammad80@gmail.com>
2022-04-04 07:21:13 +0430

    test commit

--- app/src/main/java/ir/blackswan/travelapp/MainActivity.java
+++ app/src/main/java/ir/blackswan/travelapp/MainActivity.java
@@ -28,8 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
