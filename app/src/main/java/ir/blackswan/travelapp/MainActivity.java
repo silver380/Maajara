8b5c34a6bef434b8fa1a4f5026ebf702eb537c3d
MohammadFarzan <farzanmohammad80@gmail.com>
2022-04-05 16:19:33 +0430

    main bottom navigation

--- app/src/main/java/ir/blackswan/travelapp/MainActivity.java
+++ app/src/main/java/ir/blackswan/travelapp/MainActivity.java
@@ -1,18 +1,14 @@
 package ir.blackswan.travelapp;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import ir.blackswan.travelapp.databinding.ActivityMainBinding;
 
@@ -20,17 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
     }
