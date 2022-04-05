6a7443c0b36eb074cef82893ef902e8900e19a88
Sara <sara80.kehtari@gmail.com>
2022-04-05 12:47:18 -0700

    AddTourActivity

---
+++ app/src/main/java/ir/blackswan/travelapp/AddTourActivity.java
@@ -0,0 +1,14 @@
+package ir.blackswan.travelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tour);
+    }
+}
