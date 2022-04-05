23cefea1e791af91bdea168b45c07672dc31cac0
MohammadFarzan <farzanmohammad80@gmail.com>
2022-04-06 00:04:28 +0430

    activity_add_tour completing... - update colors

--- app/src/main/java/ir/blackswan/travelapp/MainActivity.java
+++ app/src/main/java/ir/blackswan/travelapp/MainActivity.java
@@ -1,15 +1,15 @@
 package ir.blackswan.travelapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import ir.blackswan.travelapp.Utils.Toast;
 import ir.blackswan.travelapp.databinding.ActivityMainBinding;
@@ -25,10 +25,14 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        startActivity(new Intent(this , AddTourActivity.class));
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getResources().getColor(R.color.colorMainStatusBar , getTheme()));
+
+        startActivity(new Intent(this, AddTourActivity.class));
 
 
-        Toast.makeText(this , "هعی"  , Toast.LENGTH_LONG , Toast.TYPE_ERROR).show();
+        Toast.makeText(this, "هعی", Toast.LENGTH_LONG, Toast.TYPE_ERROR).show();
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host);
         NavController navCo = navHostFragment.getNavController();
