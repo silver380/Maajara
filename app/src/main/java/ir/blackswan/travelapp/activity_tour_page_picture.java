503f97731cc4caa7b7d1b5f7ceacb0a83d2169ae
Sara <sara80.kehtari@gmail.com>
2022-04-09 18:10:05 -0700

    activity_tour_page_picture added

---
+++ app/src/main/java/ir/blackswan/travelapp/activity_tour_page_picture.java
@@ -0,0 +1,14 @@
+package ir.blackswan.travelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_tour_page_picture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tour_page_picture);
+    }
+}
