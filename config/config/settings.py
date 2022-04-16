a6992ffce17687f8dcef00be1ebd7c5fc02e8057
Javad <mjavad.jafari80@gmail.com>
2022-04-17 04:23:24 +0430

    requirements.txt changes

--- config/config/settings.py
+++ config/config/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 # Password validation
