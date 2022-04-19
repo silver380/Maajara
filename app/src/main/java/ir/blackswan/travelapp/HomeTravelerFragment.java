f9a694ff19c0d1a79774bf64b4c1d11c2fa66a61
Sara <sara80.kehtari@gmail.com>
2022-04-19 11:16:56 -0700

    fragment_home_traveler.xml added

---
+++ app/src/main/java/ir/blackswan/travelapp/HomeTravelerFragment.java
@@ -0,0 +1,14 @@
+package ir.blackswan.travelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeTravelerFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home_traveler);
+    }
+}
