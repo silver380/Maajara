1acf25e1df360c6465a6b2ca133f557bbe848677
Javad <mjavad.jafari80@gmail.com>
2022-05-05 01:00:00 +0430

    fix

--- config/TravelPlan/models.py
+++ config/TravelPlan/models.py
@@ -1,8 +1,4 @@
-from enum import unique
-from logging import NOTSET
-from tkinter import CASCADE
 from django.db import models
-#from config.MyUser.models import MyUser
 
 
 class TravelPlan(models.Model):
@@ -13,17 +9,19 @@ class TravelPlan(models.Model):
     end_date = models.DateField(blank=False, null=True)
     plan_creator = models.ForeignKey('MyUser.MyUser', on_delete=models.CASCADE)
     wanted_list = models.TextField(null=True)
-    accepted_price = models.IntegerField(blank=True,null=True)
+    accepted_price = models.IntegerField(blank=True, null=True)
     # pending_leaders = models.ManyToManyField('MyUser.MyUser', related_name='pending_leaders', blank=True,
     # null=True)  # TODO: it should change
     # confirmed_leader = models.ManyToManyField('MyUser.MyUser', related_name='confirmed_leader', blank=True,
     # null=True)
-    pending_leaders = models.ManyToManyField('MyUser.MyUser',through='TravelPlanReq',related_name='pending_leaders', blank=True, null=True)
+    pending_leaders = models.ManyToManyField('MyUser.MyUser', through='TravelPlanReq', related_name='pending_leaders',
+                                             blank=True, null=True)
+
 
 class TravelPlanReq(models.Model):
     class Meta:
-        unique_together = [['tour_leader','travel_plan_id']]
-    tour_leader = models.ForeignKey('MyUser.MyUser', on_delete=models.CASCADE)
-    travel_plan_id = models.ForeignKey('TravelPlan.TravelPlan', on_delete= models.CASCADE)
-    suggested_price = models.IntegerField(default=0)
+        unique_together = [['tour_leader', 'travel_plan_id']]
 
+    tour_leader = models.ForeignKey('MyUser.MyUser', on_delete=models.CASCADE)
+    travel_plan_id = models.ForeignKey('TravelPlan.TravelPlan', on_delete=models.CASCADE)
+    suggested_price = models.IntegerField(default=0)
