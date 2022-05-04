faa57120ff0feaad3787c41a2b68199fc3b74154
Parisa-Asgarzadeh <72992505+Parisa-Asgarzadeh@users.noreply.github.com>
2022-05-04 21:53:59 +0430

    TourLeader Request Activity

---
+++ app/src/main/java/ir/blackswan/travelapp/ui/Adapters/TLeaderRequestRecyclerAdapter.java
@@ -0,0 +1,5 @@
+package ir.blackswan.travelapp.ui.Adapters;
+
+public class TLeaderRequestRecyclerAdapter {
+
+}
---
+++ app/src/main/java/ir/blackswan/travelapp/ui/TLeaderRequestActivity.java
@@ -0,0 +1,16 @@
+package ir.blackswan.travelapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.blackswan.travelapp.R;
+
+public class TLeaderRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tleader_request);
+    }
+}
