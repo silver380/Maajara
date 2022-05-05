1b60e4fd8109464602b30fc41f707d9b8411adfa
Parisa-Asgarzadeh <72992505+Parisa-Asgarzadeh@users.noreply.github.com>
2022-05-06 00:06:15 +0430

    body of tl Controller and Activity

---
+++ app/src/main/java/ir/blackswan/travelapp/Controller/TourLeaderRequestController.java
@@ -0,0 +1,12 @@
+package ir.blackswan.travelapp.Controller;
+
+import ir.blackswan.travelapp.ui.AuthActivity;
+
+public class TourLeaderRequestController extends Controller {
+
+
+
+    public TourLeaderRequestController(AuthActivity authActivity) {
+        super(authActivity);
+    }
+}
--- app/src/main/java/ir/blackswan/travelapp/ui/TLeaderRequestActivity.java
+++ app/src/main/java/ir/blackswan/travelapp/ui/TLeaderRequestActivity.java
@@ -2,15 +2,41 @@ package ir.blackswan.travelapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+
+import ir.blackswan.travelapp.Data.PlanRequest;
 import ir.blackswan.travelapp.R;
+import ir.blackswan.travelapp.databinding.ActivityTleaderRequestBinding;
 
 public class TLeaderRequestActivity extends AppCompatActivity {
+    private ActivityTleaderRequestBinding binding;
+    private PlanRequest[] pending_tl;
+    private PlanRequest[] confirmed_tl;
+    ArrayList<PlanRequest> all_tl;
+    Intent intent;
+    int plan_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityTleaderRequestBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tleader_request);
+    }
+
+    private void setRecyclers(){
+
+    }
+
+    private void setPending_tlRecycler(){
+
+    }
+
+    private void setConfirmed_tlRecycler(){
+
     }
 }
