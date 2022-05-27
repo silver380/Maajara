6418b1a7e7d406d7dbfc800300c2da10ee965320
Fateme Shafiee <61053360+fatemeshafiee@users.noreply.github.com>
2022-05-27 17:26:58 +0430

    RegisterAndAuthenticateTest

--- config/MyUser/tests.py
+++ config/MyUser/tests.py
@@ -2,3 +2,72 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
 
+class RegisterAndAuthenticateTest(APITestCase):
+
+	def test_should_not_register_invalid_email(self):
+		user_info = {
+		"email":"uu.com",
+		"password":123 ,
+		"first_name":"User",
+		"last_name" :"User"}
+		
+		response = self.client.post(('/auth/register/'),user_info)
+		self.assertEqual(response.status_code, 400)
+		
+	def test_should_not_register_invalid_password(self):
+		user_info = {
+		"email":"uu.com",
+		"password":"",
+		"first_name":"User",
+		"last_name" :"User"}
+		
+		response = self.client.post(('/auth/register/'),user_info)
+		self.assertEqual(response.status_code, 400)
+
+	def test_should_register(self):
+		user_info = {
+		"email":"u@u.com",
+		"password":"usER!@123",
+		"first_name":"User",
+		"last_name" :"User"}
+		
+		response = self.client.post(('/auth/register/'),user_info)
+		self.assertEqual(response.status_code, 201)
+
+
+	def test_should_get_token(self):
+		user_info = {
+		"email":"u@u.com",
+		"password":"usER!@123",
+		"first_name":"User",
+		"last_name" :"User"}
+		
+		self.client.post(('/auth/register/'),user_info)
+		response = self.client.post(('/auth/token/'), {"username":"u@u.com",
+				                                       "password":'usER!@123'})
+
+		self.assertEqual(response.status_code, 200)
+
+	def test_should_not_get_token_invalid_username(self):
+		user_info = {
+		"email":"u@u.com",
+		"password":"usER!@123",
+		"first_name":"User",
+		"last_name" :"User"}
+		
+		self.client.post(('/auth/register/'),user_info)
+		response = self.client.post(('/auth/token/'), {"username":"u@a.com",
+				                                       "password":'usER!@123'})
+		self.assertEqual(response.status_code, 400)
+
+	def test_should_not_get_token_invalid_password(self):
+		user_info = {
+		"email":"u@u.com",
+		"password":"usER!@123",
+		"first_name":"User",
+		"last_name" :"User"}
+		
+		self.client.post(('/auth/register/'),user_info)
+		response = self.client.post(('/auth/token/'), {"username":"u@u.com",
+				                                       "password":'usER!@12'})
+		self.assertEqual(response.status_code, 400)
--- config/Tour/tests.py
+++ config/Tour/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 # import pdb
 # pdb.set_trace()
+
 class TourListAPIViewTest(APITestCase):
 
 	def authenticate(self):
@@ -28,4 +29,21 @@ class TourListAPIViewTest(APITestCase):
 		response = self.client.get('/tour/all/')
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+
+class AddTourTest(APITestCase):
+
+		def authenticate(self):
+			user_info = {
+			"email":"u@u.com",
+			"password":123 ,
+			"first_name":"User",
+			"last_name" :"User"}
+			
+			self.client.post(('/auth/register/'),user_info)
+			response = self.client.post(('/auth/token/'), {"username":"u@u.com",
+				                                           "password":123})
+
+			self.client.credentials(HTTP_AUTHORIZATION='Token ' + response.data['token'])
+
+		#TODO
 
