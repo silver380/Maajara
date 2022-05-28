9124f9ace0253f73aaa0b2cb03ee29fec04cc150
Javad <mjavad.jafari80@gmail.com>
2022-05-28 15:14:14 +0430

    TravelPlan manager/BUGS

--- config/TravelPlan/models.py
+++ config/TravelPlan/models.py
@@ -1,10 +1,23 @@
+from datetime import datetime
+
 from django.db import models
 
 
+class TravelPlanManager(models.Manager):
+    def active(self):
+        now = datetime.now()
+        return self.get_queryset().filter(start_date__gte=now)
+
+    def inactive(self):
+        now = datetime.now()
+        return self.get_queryset().filter(start_date__lt=now)
+
+
 class TravelPlan(models.Model):
+    objects = TravelPlanManager()
     travel_plan_id = models.AutoField(primary_key=True)
     destination = models.CharField(max_length=60, blank=False, default='')
-    #TODO places should be added
+    # TODO places should be added
     start_date = models.DateField(blank=False, null=True)
     end_date = models.DateField(blank=False, null=True)
     plan_creator = models.ForeignKey('MyUser.MyUser', on_delete=models.CASCADE, related_name='plan_creator')
@@ -14,6 +27,7 @@ class TravelPlan(models.Model):
     confirmed_tour_leader = models.ForeignKey('MyUser.MyUser', on_delete=models.CASCADE, null=True)
     accepted_price = models.IntegerField(blank=True, null=True)
     places = models.ManyToManyField('Place.Place', related_name='TravelPlanPlaces', blank=True, null=True)
+
 
 class TravelPlanReq(models.Model):
     class Meta:
