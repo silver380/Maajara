0d2c79a779acc4ab195f4f4916cfdc4ceec8f3a7
Fateme Shafiee <61053360+fatemeshafiee@users.noreply.github.com>
2022-05-29 14:22:17 +0430

    ArchivedTourTL ArchivedTourUser

--- config/Tour/permissions.py
+++ config/Tour/permissions.py
@@ -13,7 +13,7 @@ class CanRate(BasePermission):
         current_tour = Tour.objects.get(pk=request.data['tour_id'])
         current_date = datetime.date.today() 
         current_tour_rate = TourRate.objects.filter(user_id=request.user.user_id,
-         tour_id=request.data['tour_id']).values()
+        tour_id=request.data['tour_id']).values()
         already_rated = current_tour_rate.exists()
         return (current_tour.end_date < current_date) and \
         (request.user in current_tour.confirmed_users.all()) and \
