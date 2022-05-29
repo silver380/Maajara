dac5fcad8ebc199c906f71be70e1629d26000fd6
Javad <mjavad.jafari80@gmail.com>
2022-05-29 16:15:59 +0430

    email

--- config/MyUser/views.py
+++ config/MyUser/views.py
@@ -44,9 +44,16 @@ class UserInfo(RetrieveAPIView):
 
 class ActivateUser(APIView):
     def get(self, request, uid, token):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0',
+            'ACCEPT': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'ACCEPT-ENCODING': 'gzip, deflate, br',
+            'ACCEPT-LANGUAGE': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+            'REFERER': 'https://www.google.com/'
+        }
         payload = json.dumps({'uid': uid, 'token': token})
         url = "http://localhost:8000/auth/users/activation/"
-        response = requests.post(url, data=payload)
+        response = requests.post(url, data=payload, headers=headers)
         if response.status_code == 204:
             return render(request, 'activation_success.html')
         else:
